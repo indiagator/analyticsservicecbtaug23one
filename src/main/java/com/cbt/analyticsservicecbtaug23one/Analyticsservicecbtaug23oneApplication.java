6afc17b4191b8f8d72f98736463a23d2dbff20d8
Prateek Kishore Karnad <trainer.it.ssk@gmail.com>
2023-08-02 10:19:31 +0530

    initial

---
+++ src/main/java/com/cbt/analyticsservicecbtaug23one/Analyticsservicecbtaug23oneApplication.java
@@ -0,0 +1,13 @@
+package com.cbt.analyticsservicecbtaug23one;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Analyticsservicecbtaug23oneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Analyticsservicecbtaug23oneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cbt/analyticsservicecbtaug23one/Analyticsservicecbtaug23oneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cbt.analyticsservicecbtaug23one;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Analyticsservicecbtaug23oneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
