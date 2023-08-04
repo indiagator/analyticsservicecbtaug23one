d580cc696d4aeafb8aa8823c446d2ef84edf99d2
Prateek Kishore Karnad <trainer.it.ssk@gmail.com>
2023-08-04 10:32:46 +0530

    Update MainRestController.java

--- src/main/java/com/cbt/analyticsservicecbtaug23one/MainRestController.java
+++ src/main/java/com/cbt/analyticsservicecbtaug23one/MainRestController.java
@@ -1,9 +1,18 @@
 package com.cbt.analyticsservicecbtaug23one;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/v1")
-public class MainRestController {
+public class MainRestController
+{
+    @GetMapping("get/instance")
+    public ResponseEntity<String> getInstance()
+    {
+        return new ResponseEntity<>("this is from ANALYTICS_SERVICE_ONE", HttpStatus.OK);
+    }
 }
